45def90aa784842f31eba7964be873a31e53f1cb
statnmap <statnmap@users.noreply.github.com>
2018-06-08 22:26:46 +0200

    update doc get_dependencies

--- R/get_dependencies.R
+++ R/get_dependencies.R
@@ -1,26 +1,29 @@
-#' return all package dependencies from current package
+#' Return all package dependencies from current package
 #'
 #' @param path path to the DESCRIPTION file
 #' @param dput if FALSE return a vector instead of dput output
 #' @param field DESCRIPTION fied to parse, Import and Depends by default
 #'
+#' @return A character vector with packages names
+#'
 #' @export
 #'
 #' @examples
 #' \dontrun{
-#' list_imports()
+#' get_dependencies()
 #' }
-#' @importFrom magrittr %>%
-get_dependencies <- function(path="DESCRIPTION",dput=TRUE,field=c('Depends','Imports')){
-
-   out <- read.dcf(path)
-   out <- out[,intersect(colnames(out),field)] %>%
-    gsub(pattern = "\n",replacement = "") %>%
+get_dependencies <- function(path = "DESCRIPTION", dput = TRUE,
+                             field = c("Depends", "Imports")) {
+  out <- read.dcf(path)
+  out <- out[, intersect(colnames(out), field)] %>%
+    gsub(pattern = "\n", replacement = "") %>%
     strsplit(",") %>%
     unlist() %>%
     setNames(NULL)
   out <- out[!grepl("^R [(]", out)]
 
-  if ( !dput ){return(out)}
+  if (!dput) {
+    return(out)
+  }
   out %>% dput()
 }
