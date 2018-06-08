5647708b484d3b3232a159297ff3c3dac3de14ca
statnmap <statnmap@users.noreply.github.com>
2018-06-08 22:18:41 +0200

    add author

--- devstuff_history.R
+++ devstuff_history.R
@@ -9,20 +9,19 @@ usethis::use_code_of_conduct()
 library(desc)
 library(glue)
 
-fill_desc_generic <- function(name, Title,
-                              Description = "Here.",
-                              repo = name,
-                              first_name = "Vincent",
-                              last_name = "Guyader",
-                              github_user = "Thinkr-open",
-                              email = "vincent@thinkr.fr",
-                              role = "c('cre', 'aut')",
-                              version = "0.0.0.9000"){
+fill_desc_generic <- function(
+  name, Title,
+  Description = "Here.",
+  repo = name,
+  dev = c("person('Vincent', 'Guyader', email = 'vincent@thinkr.fr', role = c('cre', 'aut'))",
+          "person('Sébastien', 'Rochette', email = 'sebastien@thinkr.fr', role = c('aut'))"),
+  github_user = "Thinkr-open",
+  version = "0.0.0.9000")
+{
   unlink("DESCRIPTION")
   my_desc <- desc::description$new("!new")
   my_desc$set("Package", name)
-  my_desc$set("Authors@R",
-              glue("person('{first_name}', '{last_name}', email = '{email}', role = {role})"))
+  my_desc$set("Authors@R", glue("c(", collapse(dev, sep = ",\n\t\t"), ")"))
   my_desc$del("Maintainer")
   my_desc$set_version(version)
   my_desc$set(Title = Title)
@@ -32,7 +31,7 @@ fill_desc_generic <- function(name, Title,
   my_desc$write(file = "DESCRIPTION")
 }
 fill_desc_generic(name = "attachment",
-                  Description  = "Tools to help to manage dependencies during package developement.",
+                  Description = "Tools to help to manage dependencies during package developement.",
                   Title = "Deal with dependencies")
 
 usethis::use_pipe()
