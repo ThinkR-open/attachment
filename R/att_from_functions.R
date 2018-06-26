8947987dee436a77017cf371cf487968310b749d
statnmap <statnmap@users.noreply.github.com>
2018-06-26 18:26:00 +0200

    Correct pkg::fun extract_all

--- R/att_from_functions.R
+++ R/att_from_functions.R
@@ -16,7 +16,7 @@ att_from_functions <- function(path = "R") {
   files <- list.files(path, full.names = TRUE)
   lapply(files, function(f) readLines(f) %>%
            .[grep("^#'", ., invert = TRUE)] %>%
-      stringr::str_extract_all("[\\w]+.[\\w]+(?=::)") %>%
+      stringr::str_extract_all("[[:alnum:]\\.]+(?=::)") %>%
       unlist() %>% unique()
   ) %>% unlist()
 }
