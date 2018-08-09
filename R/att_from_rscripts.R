bea8070d966d4c975727836560326fb647639549
Vincent <vincent@thinkr.fr>
2018-08-09 13:56:17 +0200

    add pattern to only scrap .R and .r file in att_from_rscripts

--- R/att_from_rscripts.R
+++ R/att_from_rscripts.R
@@ -35,11 +35,12 @@ att_from_rscript <- function(file) {
 #' Look for functions called with `::` and library/requires in folder of scripts
 #'
 #' @param path directory with R scripts inside
+#' @param pattern pattern to detect R script file
 #'
 #' @export
 
-att_from_rscripts <- function(path = "R") {
-  all_f <- list.files(path, full.names = TRUE)
+att_from_rscripts <- function(path = "R",pattern = "*.(r|R)") {
+  all_f <- list.files(path, full.names = TRUE,pattern = pattern)
   lapply(all_f, att_from_rscript) %>%
     unlist() %>%
     unique() %>%
