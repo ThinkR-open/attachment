e332c03bcbd41a8fa7815ef24f431d07f77aa161
Vincent <vincent@thinkr.fr>
2018-08-11 19:03:47 +0200

    general Doc

---
+++ R/attachment-package.R
@@ -0,0 +1,11 @@
+#' Tools to deal with package dependencies
+#'
+#' The goal of attachment is to help to deal with package dependencies during package development. It also give usefull tools to install or list missing packages used inside Rscripts or Rmds.
+#'
+#' @name attachment-package
+#' @aliases attachment-package attachment
+#' @docType package
+#' @author Vincent Guyader <vincent@@thinkr.fr>
+#' @author Sébastien Rochette <sebastien@@thinkr.fr>
+#' @keywords internal
+"_PACKAGE"
