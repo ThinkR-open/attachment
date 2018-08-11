b45008d5b139d47df1d74b27f0d5d9d21cb14615
Vincent <vincent@thinkr.fr>
2018-08-11 23:12:14 +0200

    unit testing

---
+++ tests/testthat/test-rmd.R
@@ -0,0 +1,31 @@
+context("test-rmd.R")
+
+test_that("rmd well parsed", {
+
+  res  <-sort(attachment::att_from_rmds(path = "."))
+
+
+  expect_equal(sort(res),
+
+               sort(
+                 c("data.table",
+                   "attachment",
+                   "knitr",
+                   "findme1",
+                   "findme2",
+                   "findme3",
+                   "findme4",
+                   "findme5",
+                   "findme6",
+                   "findme1a",
+                   "findme2a",
+                   "findme3a",
+                   "findme4a",
+                   "findme5a",
+                   "findme6a"
+                 )
+
+               ))
+
+
+  })
---
+++ tests/testthat/test-rscript.R
@@ -0,0 +1,31 @@
+context("test-rscript.R")
+
+test_that("rscript well parsed", {
+
+  res  <-sort(attachment::att_from_rscript(path = "f2.R"))
+
+
+  expect_equal(sort(res),
+
+               sort(
+                 c("data.table",
+                   "attachment",
+                   "knitr",
+                   "findme1",
+                   "findme2",
+                   "findme3",
+                   "findme4",
+                   "findme5",
+                   "findme6",
+                   "findme1a",
+                   "findme2a",
+                   "findme3a",
+                   "findme4a",
+                   "findme5a",
+                   "findme6a"
+                 )
+
+               ))
+
+
+})
