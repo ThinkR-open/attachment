759b967d20d4903ae029a0bc0538d47eeb8c2ef7
Vincent <vincent@thinkr.fr>
2018-09-03 00:03:47 +0200

    introduction d'un :: dans le .R

--- inst/dummypackage/R/my_mean.R
+++ inst/dummypackage/R/my_mean.R
@@ -1,12 +1,12 @@
-
 #' my_mean
 #'
 #' @param x a vector
 #'
 #' @export
 #' @importFrom utils na.omit
-#' @importFrom magrittr %>% 
+#' @importFrom magrittr %>%
 my_mean <- function(x){
-  x <- x %>% na.omit()
+  x <- x %>% utils::na.omit()
+  1+1
   sum(x)/length(x)
 }
