1090d6013ff597bffb75fd325cc2dfb8ca9b301b
statnmap <statnmap@users.noreply.github.com>
2019-05-15 08:54:06 +0200

    add package level documentation

--- R/attachment-package.R
+++ R/attachment-package.R
@@ -1,11 +1,8 @@
-#' Tools to deal with package dependencies
-#'
-#' The goal of attachment is to help to deal with package dependencies during package development. It also give useful tools to install or list missing packages used inside Rscripts or Rmds.
-#'
-#' @name attachment-package
-#' @aliases attachment-package attachment
-#' @docType package
-#' @author Vincent Guyader <vincent@@thinkr.fr>
-#' @author Sébastien Rochette <sebastien@@thinkr.fr>
 #' @keywords internal
 "_PACKAGE"
+
+# The following block is used by usethis to automatically manage
+# roxygen namespace tags. Modify with care!
+## usethis namespace: start
+## usethis namespace: end
+NULL
