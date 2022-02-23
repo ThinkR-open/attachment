290a0809f373801af06efd759892ee235cf24313
Vincent Guyader <vincent@thinkr.Fr>
2022-02-23 17:47:59 +0000

    test on test

--- tests/testthat/test-renv_create.R
+++ tests/testthat/test-renv_create.R
@@ -8,10 +8,21 @@ dummypackage <- file.path(tmpdir, "dummypackage")
 print("ici")
 print(dummypackage)
 # Sys.sleep(525)
-my_renv <- create_renv_for_dev(path = dummypackage,install_if_missing = FALSE)
-my_renv
+my_renv_golem <- create_renv_for_dev(path = dummypackage,install_if_missing = FALSE,dev_pkg = "golem")
+my_renv_golem
 
 
-local_renv <- getFromNamespace("lockfile", "renv")(my_renv)
-les_pkg <- local_renv$data()$Packages
+local_renv_golem <- getFromNamespace("lockfile", "renv")(my_renv_golem)
+local_renv_golem
+les_pkg <- names(local_renv_golem$data()$Packages)
+
+
+test_that("dev_pkg works", {
+testthat::expect_true("golem" %in% les_pkg)
+})
+test_that("create_renv_for_dev works", {
 testthat::expect_equal_to_reference(names(les_pkg),"my_renv.test")
+})
+
+
+
